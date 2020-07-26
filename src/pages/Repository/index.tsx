4c9200e9fb8fccc1096de53bbe380c513d00c2bd
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-26 11:43:48 -0300

    Estilizando Detalhe
    Consertei a importação da fonte Roboto, não é recomendado usar @import e sim usar <Link>, pois as APIs CSSOM que usam na produção não lidam bem com isso.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
     repository: string;
@@ -8,7 +12,57 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository: {params.repository}</h1>;
-}
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/dashboard">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img
+                        src=""
+                        alt=""
+                    />
+                    <div>
+                        <strong>camillacorreia</strong>
+                        <p>sdgffgydsfydf</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>656464</strong>
+                        <span>Stars</span>
+                    </li>
+
+                    <li>
+                        <strong>6564</strong>
+                        <span>Forks</span>
+                    </li>
+
+                    <li>
+                        <strong>6656</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </ RepositoryInfo>
+
+            <Issues>
+                <Link to="/">
+                    <div>
+                        <strong>dsdsdgsdggd</strong>
+                        <p>dsdd</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </ Issues>
+        </>
+    );
+};
 
 export default Repository;
