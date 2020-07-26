f542324a43445a5a2a170bbc77d7de5dc352b824
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-26 11:10:15 -0300

    Navegação entre Rotas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
@@ -77,18 +78,18 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="">
-                    <img
-                        src={repository.owner.avatar_url}
-                        alt={repository.owner.login}
-                    />
-                    <div>
-                        <strong>{repository.full_name}</strong>
-                        <p>{repository.description}</p>
-                    </div>
-    
-                    <FiChevronRight size={20} />
-                    </a>
+                    <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+        
+                        <FiChevronRight size={20} />
+                    </ Link>
                 ))}
             </ Repositories>
         </>
