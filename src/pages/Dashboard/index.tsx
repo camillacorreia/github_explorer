cf606b1a4d6096065c30d8d08d7cbb53fbece74f
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-26 10:33:15 -0300

    Conectando a API
    yarn add axios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,34 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState(''); // Estado para armazenar valor do input
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    // Adição de um novo repositoório
+    // Consumir API do Github
+    // Salvar novo repositório no estado
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault(); // Previne o comportamento padrão do formulário
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+
+        const repository = response.data;
+        
+        setRepositories([...repositories, repository]);
+
+        // Limpar input
+        setNewRepo('');
+    }
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
 
-            <Form>
-                <input placeholder="Digite o nome do repositório" />
+            <Form onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório" 
+                />
                 <button type="submit">Pesquisar</button>
             </ Form>
 
             <Repositories>
-                <a href="">
-                   <img
-                        src="https://avatars3.githubusercontent.com/u/50275354?s=460&u=ab6a13576a9ed46db6faa2376c5ae2311bc492d3&v=4"
-                        alt="Camilla Correia"
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="">
+                    <img
+                        src={repository.owner.avatar_url}
+                        alt={repository.owner.login}
                     />
                     <div>
-                        <strong>camillacorreia/github_explorer</strong>
-                        <p>Explore repositórios incríveis do github - feito com ReactJS, TypeScript e styled-components.</p>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
                     </div>
-
+    
                     <FiChevronRight size={20} />
-                </a>
+                    </a>
+                ))}
             </ Repositories>
         </>
     );
