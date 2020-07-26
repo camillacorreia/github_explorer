c3b310a10c8129228e5ac807e941f8da6cfc5426
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-26 01:08:45 -0300

    Estilizando Dashboard
    Polished: Permite trabalhar com cores
    yarn add polished
    yarn add react-icons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>
-}
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </ Form>
+
+            <Repositories>
+                <a href="">
+                   <img
+                        src="https://avatars3.githubusercontent.com/u/50275354?s=460&u=ab6a13576a9ed46db6faa2376c5ae2311bc492d3&v=4"
+                        alt="Camilla Correia"
+                    />
+                    <div>
+                        <strong>camillacorreia/github_explorer</strong>
+                        <p>Explore repositórios incríveis do github - feito com ReactJS, TypeScript e styled-components.</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </ Repositories>
+        </>
+    );
+};
 
 export default Dashboard;
